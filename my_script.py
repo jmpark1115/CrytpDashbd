77f2ef9bd96386b330ad4ba21077628f835489df
jmpark1115 <jmpark1115@gmail.com>
2024-02-22 18:01:21 +0900

    okx script 표현

--- my_script.py
+++ my_script.py
@@ -19,7 +19,7 @@ script = """
         } else {
             clearInterval(_objCheckOkxSession);
         }
-        _objCheckOkxSession = setInterval(getOkxInstruments, 5000)
+        _objCheckOkxSession = setInterval(getOptionTickers(), 5000)
       }
 
       function getOkxInstruments() {
@@ -49,7 +49,91 @@ script = """
         }); //ajax
       } // getOkxInstruments
 
-doOpenOkxRestApi();
+      async function getIndexTickers() {
+        console.log('getIndexTickers called');
+        const path = '/api/v5/market/index-tickers';
+        const response = await $.ajax({
+                //async: true,
+                url: API_URL + path,
+                type: 'GET',
+                data: {
+                     'instId' : 'BTC-USD'
+                },
+                dataType: 'json',
+        });
+        const insts = response.data;
+        if (typeof insts === 'object') {
+           let idxPx = parseFloat(insts[0].idxPx);
+           console.log('idxPx = ' + idxPx);
+           return idxPx;
+        }
+      } //getIndexTickers
 
+      async function getOptionTickers() {
+        console.log('getOptionTickers callled');
+        const path = '/api/v5/market/tickers';
+        tickers = {};
+        var idxPx = await getIndexTickers();
+        console.log('!!! result of idxPx: ' + idxPx);
+        if (typeof idxPx == 'undefined') {
+            return tickers;
+        }
+        const response = await $.ajax({
+            //async: true,
+            url: API_URL + path,
+            type: 'GET',
+            data: {
+                 'instType' : 'OPTION',
+                 'uly' : 'BTC-USD'
+            },
+            dataType: 'json',
+        });
+
+        console.log('success_' + response.code);
+        const insts = response.data;
+        if (typeof insts === 'object') {
+            if (idxPx <= 0.0) {
+                return tickers;
+            }
+        }
+        for(let i=0; i<insts.length; i++) {
+            let ticker = insts[i]['instId'].split('-');
+            let coin = ticker[0];
+            let expire_data = ticker[2];
+            let strike = ticker[3];
+            let side   = ticker[4];
+            let refine_info = {};
+            refine_info.askPrice = parseFloat(insts[i].askPx)*idxPx || 0;
+            refine_info.askQty = parseFloat(insts[i].askSz) || 0;
+            refine_info.bidPrice = parseFloat(insts[i].bidPx)*idxPx || 0;
+            refine_info.bidQty = parseFloat(insts[i].bidSz) || 0;
+            refine_info.timestamp = parseInt(insts[i].ts) || 0;
+
+            if(!tickers.hasOwnProperty(coin)) {
+                tickers[coin] = {};
+            }
+            if(!tickers[coin].hasOwnProperty(expire_data)) {
+                tickers[coin][expire_data] = {};
+            }
+            if(!tickers[coin][expire_data].hasOwnProperty(strike)) {
+                tickers[coin][expire_data][strike] = {};
+            }
+            if(!tickers[coin][expire_data][strike].hasOwnProperty(side)) {
+                tickers[coin][expire_data][strike][side] = {};
+            }
+            tickers[coin][expire_data][strike][side] = refine_info
+        } //for
+        return tickers;
+      } //getOptionTickers
+
+    async function main() {
+      await getIndexTickers();
+      const intervalId = setInterval(async () => {
+        await getOptionTickers();
+      }, 5000);
+      console.log('Interval started with ID:', intervalId);
+    }
+
+main();
 </script>
 """
