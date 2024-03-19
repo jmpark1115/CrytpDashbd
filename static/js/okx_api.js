a93bb2e22496a8854a82b90d0560d3c6df1682a6
jmpark1115 <jmpark1115@gmail.com>
2024-03-19 16:45:55 +0900

    okx socket 방식 구현

--- static/js/okx_api.js
+++ static/js/okx_api.js
@@ -131,4 +131,120 @@
       console.log('Interval started with ID:', intervalId);
     }
 
-//main();
+////////////////////////////////////////////////////////////////
+var Tickers = {};
+/*
+https://www.okx.com/docs-v5/en/#overview-websocket
+*/
+    var _wSocket_okx_ticker;
+    const _okx_URL = 'wss://ws.okx.com:8443';
+    const _okx_ticker_endpoint = '/ws/v5/public';
+
+    function _okx_ticker_init() {
+        wsAddress = _okx_URL + _okx_ticker_endpoint;
+        _wSocket_okx_ticker = new WebSocket(wsAddress);
+
+        _wSocket_okx_ticker.onopen = function(e) { _okx_ticker_open(e) };
+        _wSocket_okx_ticker.onclose = function(e) { _okx_ticker_close(e) };
+        _wSocket_okx_ticker.onmessage = function(e) { _okx_ticker_message(e) }
+        _wSocket_okx_ticker.onerror = function(e) { _okx_ticker_error(e) };
+    }
+
+    function doOpenOkxTicker() {
+        //console.log('====> doOpenBinanceOrderbook');
+        _okx_ticker_init();
+    }
+
+    //okx ticker websocket open
+    function _okx_ticker_open() {
+        console.log("okx ticker WebSocket opened!");
+        const subscribe_data = {
+            "op": "subscribe",
+            "args": [
+                {
+                    "channel": "opt-summary",
+                    "instFamily": "BTC-USD"
+
+                    //"channel": "tickers",
+                    //"instFamily" : "BTC-USD" //-> ng
+                    //"instFamily" : "BTCUSD" //-> ng
+
+                    //"channel": "options",
+                    //"instType": "OPTION",
+                    //"instFamily" : "BTC-USD",
+                    //"instId": "BTCUSD"
+                }
+            ]
+        }
+        _wSocket_okx_ticker.send(JSON.stringify(subscribe_data));
+    }
+
+    function _okx_ticker_message(e) {
+        //console.log( "okx ticker message : " + e.data );
+        //JSON Object 변환 및 추출
+        TickerData = JSON.parse(e.data);  //json 객체로 변환
+        console.log("okx ticker length : " + TickerData.data.length); // attribte 호출 ok
+        console.log("okx ticker MESSAGE : " + JSON.stringify(TickerData.data[0])); //문자열 + 문자열(json 문자열화)
+        //console.log("okx tikcer msg : " + TickerData.data[0]); 문자열 + 객체 -> ng
+        //console.log(TickerData.data.length);
+        //console.log(TickerData.data[0]);
+        okxGetOptionTickers(TickerData.data);
+    }
+
+    //okx ticker websocket error process
+    function _okx_ticker_error(e) {
+        console.log( "okx ticker WebSocket ERROR : " + e.data );
+    }
+
+    //okx ticker websocket error process
+    function _okx_ticker_close() {
+        _wSocket_okx_ticker.close();
+        console.log("okx ticker WebSocket closed!");
+        console.log("okx ticker WebSocket will reopen!");
+        doOpenOkxTicker();
+    }
+
+    function okxGetOptionTickers(data) {
+        var tickers = {};
+        const insts = data;
+        for (let i=0; i<insts.length; i++) {
+                let ticker = insts[i]['instId'].split('-');
+                let coin = ticker[0];
+                let expire_data = ticker[2];
+                let strike = ticker[3];
+                let side   = ticker[4];
+                let refine_info = {};
+                refine_info.askPrice = getRandomIntInRange(150, 200);
+                refine_info.askQty = parseFloat(insts[i].askVol) || 0;
+                refine_info.bidPrice = getRandomIntInRange(100, 150);;
+                refine_info.bidQty = parseFloat(insts[i].bidVol) || 0;
+                refine_info.timestamp = parseInt(insts[i].ts) || 0;
+                refine_info.diff = refine_info.askPrice - refine_info.bidPrice;
+                refine_info.remainDate = getRandomIntInRange(5, 15);
+
+                if(!tickers.hasOwnProperty(coin)) {
+                    tickers[coin] = {};
+                }
+                if(!tickers[coin].hasOwnProperty(expire_data)) {
+                    tickers[coin][expire_data] = {};
+                }
+                if(!tickers[coin][expire_data].hasOwnProperty(strike)) {
+                    tickers[coin][expire_data][strike] = {};
+                }
+                if(!tickers[coin][expire_data][strike].hasOwnProperty(side)) {
+                    tickers[coin][expire_data][strike][side] = {};
+                }
+                tickers[coin][expire_data][strike][side] = refine_info
+        }//for
+        if (tickers) {
+            //Tickers = {'okx': tickers};
+            Tickers = tickers;
+            console.log('okx ticker updated');
+            return true;
+        };
+        console.log('! okx ticker not updated');
+        return false;
+    }
+
+//_okx_ticker_init();
+//console.log('okx ticker started');
