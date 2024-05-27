22c91ceb23cbd80353c1395bb7799310b16ca04d
jmpark1115 <jmpark1115@gmail.com>
2024-05-27 12:45:17 +0900

    .

---
+++ config/logging_local.py
@@ -0,0 +1,58 @@
+import os
+try:
+    from .common import BASE_DIR
+except :
+    raise ValueError
+
+LOGGING = {
+    'version': 1,
+    # 기존의 로깅 설정을 비활성화 할 것인가?
+    'disable_existing_loggers': False,
+
+    # 포맷터
+    # 로그 레코드는 최종적으로 텍스트로 표현됨
+    # 이 텍스트의 포맷 형식 정의
+    # 여러 포맷 정의 가능
+    'formatters': {
+        'format1': {
+            'format': u'[%(asctime)s] [%(module)s:%(lineno)s] %(message)s',
+        },
+        'format2': {
+            'format': '[%(asctime)s] [%(module)s:%(lineno)s]  %(message)s',
+        },
+        'simple': {
+            'format': '[%(asctime)s] %(message)s',
+        },
+    },
+
+    # 핸들러
+    # 로그 레코드로 무슨 작업을 할 것인지 정의
+    # 여러 핸들러 정의 가능
+    'handlers': {
+        # 로그 파일을 만들어 텍스트로 로그레코드 저장
+        'file': {
+            'level': 'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs', 'logfile.txt'),
+            'maxBytes': 1024*1024*20, # 5 MB
+            'backupCount': 20,
+            'formatter': 'format1',
+            'encoding': 'UTF-8',
+        },
+        # 콘솔(터미널)에 출력
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'format2',
+        }
+    },
+    # 로거
+    # 로그 레코드 저장소
+    # 로거를 이름별로 정의
+    'loggers': {
+        'optionbd': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+        },
+    },
+}
